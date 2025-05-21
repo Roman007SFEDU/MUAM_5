833842f7d2b006782958383eb478772dcf0a32f5
Roman007SFEDU <rn0361123@gmail.com>
2025-05-21 13:39:14 +0300

    Добавлен класс Visualizer в ветке feature_2

--- laba_5.py
+++ laba_5.py
@@ -18,3 +18,13 @@ class Modulator:
     def fm_modulate(self,signal,time,modulation_index=1.0):
         phase=2*np.pi*self.carrier_frequency*time+modulation_index*signal
         return np.sin(phase)
+    class Visualizer:
+    def plot_signal(self,time,signal,title="Signal"):
+        plt.figure()
+        plt.plot(time,signal)
+        plt.title(title)
+        plt.xlabel("Time")
+        plt.ylabel("Amplitude")
+        plt.grid()
+        plt.show()
+
